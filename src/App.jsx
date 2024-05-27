3e24a00b309ebaeb20c9208086decf1b60dd5c6b
kim <malcomchege0582@gmail.com>
2024-05-27 23:08:36 +0300

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import "./Layout.scss"
 import Navbar from "./components/navbar/Navbar"
 import Homepage from "./routes/homepage/Homepage"
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import ListPage from "./routes/listPage/ListPage";
 function App() {
-  return (
-    <div className="layout">
-      <Navbar/>
-      <div className="content">
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: 
       <Homepage/>
-      </div>
-    </div>
+    },
+    {
+      path: "/list",
+      element: <ListPage/>,
+    },
+  ]);
+  return (
+    // <div className="layout">
+    //   <Navbar/>
+    //   <div className="content">
+    //   <Homepage/>
+    //   </div>
+    // </div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/routes/listPage/ListPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListPage() {
+  return (
+    <div>
+      <h1>list page</h1>
+    </div>
+  )
+}
+
+export default ListPage
