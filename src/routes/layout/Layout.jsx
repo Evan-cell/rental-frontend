94f837e1117311df5dff18ea034cc77712cd97b4
kim <malcomchege0582@gmail.com>
2024-05-29 10:38:38 +0300

    fixed overflow scroll bug

--- src/routes/layout/Layout.jsx
+++ src/routes/layout/Layout.jsx
@@ -4,12 +4,12 @@ import Navbar from '../../components/navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 function Layout() {
   return (
-    <div className='layout'>
+    <div className="layout">
       <div className="navbar">
-        <Navbar/>
+        <Navbar />
       </div>
       <div className="content">
-        <Outlet />
+        <Outlet/>
       </div>
     </div>
   )
--- src/routes/listPage/ListPage.jsx
+++ src/routes/listPage/ListPage.jsx
@@ -4,20 +4,21 @@ import "./ListPage.scss"
 import Filter from '../../components/filter/Filter'
 import Card from '../../components/card/Card'
 function ListPage({}) {
-  const data = listData
-  return (
-    <div className='listPage'>
-      <div className="listContainer">
-        <div className="wrapper">
-          <Filter/>
-          {data.map(item=>(
-        <Card key={item.id} item={item}/>
-      ))}
-        </div>
+  const data = listData;
+
+  return <div className="listPage">
+    <div className="listContainer">
+      <div className="wrapper">
+        <Filter/>
+        {data.map(item=>(
+          <Card key={item.id} item={item}/>
+        ))}
       </div>
-      <div className="mapContainer">map</div>
     </div>
-  )
+    <div className="mapContainer">
+      
+    </div>
+  </div>;
 }
 
 export default ListPage
